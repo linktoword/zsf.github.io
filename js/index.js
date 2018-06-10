5ba46fb1085876a335984581ec93b226c9c222f0
linktoword <15239420304@163.com>
2018-06-10 21:17:01 +0800

    图标显示完成

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-  
+  $(".icon-mail").hover(function () {
+    $(".mail").show();
+  },function(){
+    $(".mail").hide();
+  });
+  $(".icon-github").hover(function () {
+    $(".github").show();
+  }, function () {
+    $(".github").hide();
+  });
+  $(".icon-weixin").hover(function () {
+    $(".weixin").show();
+  }, function () {
+    $(".weixin").hide();
+  });
+  $(".icon-qq").hover(function () {
+    $(".qq").show();
+  }, function () {
+    $(".qq").hide();
+  });
+
 });
